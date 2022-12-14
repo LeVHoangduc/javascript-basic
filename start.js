49f8fa8b946195e5fcc7f5d8bacd5cbab9c98dcf
LeVHoangduc <levhoangduc@gmail.com>
2022-12-14 12:41:03 +0700

    source chapter2, video 1

---
+++ start.js
@@ -0,0 +1,42 @@
+/*7 data types : 
+undefined , null , boolean , string , symbol ,number and Object
+*/
+/**
+ Code OUTPUT
+ \' single quote
+ \" double quote
+ \\ backslash
+ \n newline
+ \r carriage return
+ \t tab
+ \b backspace
+ \f form feed
+ */
+//JSON
+/**
+ json.stringify() : change a type into a string 
+ */
+var a;
+a=7;
+console.log(a)
+
+var length=0;
+var name ="messi";
+length=name;
+console.log(length)
+
+// ARRAY : the elements can be any data type
+var ARRAY =[[1,2,3],[4,5,6],[7,8,9],[[10,11,12],13,14]];
+var data =ARRAY[2][1]; // number : 8
+console.log(ARRAY)
+/**
+ push() 
+ pop() : remove 
+ shift() : remove first element
+ unshift() : add and replace first element 
+ */
+
+ function name_function(argument)
+ {
+    syntax;
+ }
